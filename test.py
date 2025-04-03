baddf06b63bc2fa50be79a7642d1c9e76a47a1fb
Strenghtsd <fertyui@naver.com>
2025-04-03 21:03:16 +0900

    nsd.py

--- test.py
+++ test.py
@@ -1,6 +1,34 @@
-print("hello")
-for i in range(1, 11):
-    if i % 2:
-        print("*"*i)
-print("test")
-print("testing")
+import cv2
+import numpy as np
+import keyboard as k
+import time as t
+
+"""
+img = np.full((200, 400), 255, np.uint8)
+title = "test"
+cv2.imshow(title, img)
+
+while True:
+    key = cv2.waitKeyEx(0); print(key)
+    if key == 27: break
+    
+cv2.destroyAllWindows()
+"""
+
+"""
+flag = False
+
+while True:
+    key = k.read_key(); flag = True
+    if key == "esc":
+        break
+    else:
+        if flag:
+            print(key)
+            flag = False
+            t.sleep(0.75)
+            
+"""
+
+
+
